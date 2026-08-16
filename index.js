10a6c34d410fe93734118d206ae070c877326056
joaoleite06-blip <joaoleite06@gmail.com>
2026-08-16 15:48:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const db = require('./database');
 const pendingDiscordLogins = new Map();
 const app = express();
 
+const app = express();
+
+app.set('trust proxy', 1);
+
 app.use(cors());
-app.use(express.json());
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
